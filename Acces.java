c21b16dc765803c47ca997e300364d21bff4b684
Nassima Haimar <84714933+Nassimahmr@users.noreply.github.com>
2021-10-17 14:06:02 +0200

    Create Acces.java

---
+++ Acces.java
@@ -0,0 +1,12 @@
+public class Acces implements Runnable {
+public Interfaces inter;
+	public Acces(Interfaces location) {
+		// TODO stub constructeur généré automatiquement
+		inter = location;
+	}
+
+	@Override
+	public void run() {
+		// TODO Stub de méthode généré automatiquement
+   }
+}
